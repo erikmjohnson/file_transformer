c4539a725a94984da84da99aca7fecc9f660d7bd
erikmjohnson <erikarisma98@gmail.com>
2019-02-17 16:34:30 -0800

    adjust quotation marks

--- loop.js
+++ loop.js
@@ -4,7 +4,7 @@ const os = require('os');
 const fs = require('fs');
 
 
-const peopleNames = ["Erik", "Nikki", "Karl", "Jess", "Rick"];
+const peopleNames = ['Erik', 'Nikki', 'Karl', 'Jess', 'Rick'];
 
 const buffedOut = () => {
     peopleNames.forEach(x => console.log(Buffer.from(x)));
