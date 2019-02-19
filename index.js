d6b06d65e059b578c1b4a5ee62279e819ff9576d
erikmjohnson <erikarisma98@gmail.com>
2019-02-19 14:03:49 -0800

    New file writes into files folder

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const os = require('os');
+const fs = require('fs');
+
+
+// const peopleNames = ['Erik', 'Nikki', 'Karl', 'Jess', 'Rick'];
+//
+// const buffedOut = (array) => {
+//   array.forEach(x => {
+//     for (let j = 0; j < x.length; j++) {
+//       console.log(x.charCodeAt(j));
+//     }
+//   })
+// };
+
+// buffedOut(peopleNames);
+
+fs.writeFile(`${__dirname}/loop.js`, (error, buffer) => {
+
+})
+
